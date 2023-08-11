bbe3a2dd0fc7816e5d6bad7bd808428f555d659f
MorgachevStepan <99796851+MorgachevStepan@users.noreply.github.com>
2023-08-11 12:17:03 +0300

    Add files via upload

---
+++ src/test/java/com/example/Weather/Parser/WeatherParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Weather.Parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
